fbcebea46668b246d9f19d42ba8a02509a86ddcf
Yatsiuk Oleksandr <saj87@bk.ru>
2020-05-28 09:54:10 +0300

    Updates

---
+++ precache-manifest.e2cb36aae372e347f0adab4e54654f93.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9bde6c5e429ce48a3cf55c1c0414872",
+    "url": "/CVV_Yatsiuk_O/index.html"
+  },
+  {
+    "revision": "45028d8a8655e3b3ff8c",
+    "url": "/CVV_Yatsiuk_O/static/css/main.37be9609.chunk.css"
+  },
+  {
+    "revision": "bcd8dd3dfd77f120fbbc",
+    "url": "/CVV_Yatsiuk_O/static/js/2.1fd040ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CVV_Yatsiuk_O/static/js/2.1fd040ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45028d8a8655e3b3ff8c",
+    "url": "/CVV_Yatsiuk_O/static/js/main.ac76283e.chunk.js"
+  },
+  {
+    "revision": "436b0da850300a231271",
+    "url": "/CVV_Yatsiuk_O/static/js/runtime-main.823d5b51.js"
+  },
+  {
+    "revision": "09f56e91ecf853d30ce6ce1abb58bfd6",
+    "url": "/CVV_Yatsiuk_O/static/media/git.09f56e91.png"
+  },
+  {
+    "revision": "e154da526210b69c549e5f0be902e1e2",
+    "url": "/CVV_Yatsiuk_O/static/media/linkedin.e154da52.png"
+  },
+  {
+    "revision": "277223d5cf3363a2d9f5cd84b39045e7",
+    "url": "/CVV_Yatsiuk_O/static/media/myphoto.277223d5.jpg"
+  },
+  {
+    "revision": "a84783664dcdd4b3b723ccb0eca8b668",
+    "url": "/CVV_Yatsiuk_O/static/media/telegram.a8478366.png"
+  },
+  {
+    "revision": "da3b52a265f79c2e698644adf6bf7490",
+    "url": "/CVV_Yatsiuk_O/static/media/uk.da3b52a2.png"
+  }
+]);
